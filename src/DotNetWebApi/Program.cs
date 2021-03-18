33b14d226322b462c183d2bd3c324f14bf803816
Esteban Solano Granados <stvansolano@outlook.com>
2021-03-18 22:28:25 +0000

    Added DotNetWebApi project

---
+++ src/DotNetWebApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
